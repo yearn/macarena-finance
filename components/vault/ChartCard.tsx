ecf9925a41e5da3ad8ac379d10999e47db8f5856
Karelian Pie <devkarelianpie@gmail.com>
2023-05-01 11:00:41 +0300

    refactor: Check if undefined

--- components/vault/ChartCard.tsx
+++ components/vault/ChartCard.tsx
@@ -98,7 +98,14 @@ function	ChartCard({address, price, chainID}: {address: string, price: number, c
 					pricePerShare
 				}
 			}
-		}` : null, (query: string): any => request(networks[chainID === 1337 ? 1 : chainID  || 1].graphURI!, query)
+		}` : null, (query: string): any => {
+		const network = chainID === 1337 ? 1 : chainID  || 1;
+		const graphURI = networks[network].graphURI;
+
+		if (graphURI) {
+			request(graphURI, query);
+		}
+	}
 	);
 
 	/* 🔵 - Yearn Finance **************************************************
